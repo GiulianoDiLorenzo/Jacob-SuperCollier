ac251f6c9d661394adee8015ee029ffa58e3c118
ernestouali <146449852+ernestouali@users.noreply.github.com>
2024-05-25 15:21:48 +0200

    Add files via upload

--- Sensors/Triple_accelerometer.ino
+++ Sensors/Triple_accelerometer.ino
@@ -5,21 +5,13 @@
 #include <OSCMessage.h>
 #include <OSCBoards.h>
 
-/*
-#ifdef BOARD_HAS_USB_SERIAL
-#include <SLIPEncodedUSBSerial.h>
-SLIPEncodedUSBSerial SLIPSerial( thisBoardsSerialUSB );
-#else
-#include <SLIPEncodedSerial.h>
- SLIPEncodedSerial SLIPSerial(Serial); // Change to Serial1 or Serial2 etc. for boards with multiple serial ports that don’t have Serial
-#endif
-*/
-
+// Renaming Pins
 #define A_HR A1
 #define A_X A3
 #define A_Y A4
 #define A_Z A5
 
+// Variables to store the sensors' signals
 float sensorValue; 
 float accX;
 float accY;
@@ -27,107 +19,65 @@ float accZ;
 float timeStep = 200; //[ms]
 
 
-// Variables to hold the zero-g voltage after calibration
+// Variables to hold the zero-g voltage after calibration, see calibratesensor()
 float zeroGVoltageX = 0.0;
 float zeroGVoltageY = 0.0;
 float zeroGVoltageZ = 0.0;
 
 
-// Calibration values for 5V supply and 1.5g sensitivity setting
+// Calibration values for 5V supply and 1.5g sensitivity setting, see calibratesensor()
 const float zeroGVoltage = 2.5; // Zero-g voltage at 2.5V (assuming 5V supply)
 const float sensitivity = 0.8;  // Sensitivity (V/g) for 1.5g setting
 
-
-
+//Instantiating a object of the class accelerometer
 AcceleroMMA7361 acc;
 
 void setup() {
   Serial.begin(9600);   // serial transmission bitrate
 
+  //Setting pins as Inputs only
   pinMode(A_HR, INPUT);
   pinMode(A_X, INPUT);
   pinMode(A_Y, INPUT);
   pinMode(A_Z, INPUT);
 
+  //Calibrating the sensor
   acc.begin(13, 12, 11, 10, A_X, A_Y, A_Z);
   acc.setARefVoltage(5);  // Set the reference voltage to 3.3V
   acc.setSensitivity(HIGH);  // Set sensitivity to 1.5g (LOW for 6g)
-  //acc.calibrate();
   calibrateSensor();
 }
 
 void loop() {
-  /*
-  Serial.println("***********************");
-  Serial.print("LIBRARY (raw): ");
-  Serial.print(acc.getXRaw());
-  Serial.print(", ");
-  Serial.print(acc.getYRaw());
-  Serial.print(", ");
-  Serial.println(acc.getZRaw());
-  */
- /*
-  //Serial.print("LIBRARY (volt): ");
-  accX = (float) acc.getXVolt()/1000; 
-  Serial.print("AccX: ");
-  Serial.println(accX);
-
-  accY = (float) acc.getYVolt()/1000; 
-  Serial.print("AccY: ");
-  Serial.println(accY);
-
-
-  accZ = (float) acc.getXVolt()/1000; 
-  Serial.print("AccZ: ");
-  Serial.println(accZ);
-  */
-  
-
-/*
-  Serial.print("LIBRARY (degrees): ");
-  Serial.print(acc.getXAccel());
-  Serial.print(", ");
-  Serial.print(acc.getYAccel());
-  Serial.print(", ");
-  Serial.println(acc.getZAccel());
-*/
 
   // Read the analog values
-  int xAnalog = analogRead(A_X);
-  int yAnalog = analogRead(A_Y);
-  int zAnalog = analogRead(A_Z);
+  accX = analogRead(A_X);
+  accY = analogRead(A_Y);
+  accZ = analogRead(A_Z);
+  sensorValue = analogRead(A1);
 
   // Convert analog values to voltage
-  float xVoltage = (xAnalog * 5.0) / 1023.0;
-  float yVoltage = (yAnalog * 5.0) / 1023.0;
-  float zVoltage = (zAnalog * 5.0) / 1023.0;
+  accX = (xAnalog * 5.0) / 1023.0;
+  accY = (yAnalog * 5.0) / 1023.0;
+  accZ = (zAnalog * 5.0) / 1023.0;
 
   // Calculate acceleration in g
-  float xAcceleration = (xVoltage - zeroGVoltageX) / sensitivity;
-  float yAcceleration = (yVoltage - zeroGVoltageY) / sensitivity;
-  float zAcceleration = (zVoltage - zeroGVoltageZ) / sensitivity;
+  accX = (xVoltage - zeroGVoltageX) / sensitivity;
+  accY = (yVoltage - zeroGVoltageY) / sensitivity;
+  accZ = (zVoltage - zeroGVoltageZ) / sensitivity;
 
   // Calculate acceleration in m/s²
-  xAcceleration *= 9.81;
-  yAcceleration *= 9.81;
-  zAcceleration *= 9.81;
+  accX *= 9.81;
+  accY *= 9.81;
+  accZ *= 9.81;
 
-  // Print the results
-  /*
-  Serial.print("X acceleration : " + String(xAcceleration) + " m/s²");
-  Serial.print("Y acceleration : " + String(yAcceleration) + " m/s²");
-  Serial.print("Z acceleration : " + String(zAcceleration) + " m/s²");
-  Serial.println();
-  */
-
-  Serial.print(String(xAcceleration));
+  // Print the results on the Serial port for Processing
+  Serial.print(String(accX));
   Serial.print(",");
-  Serial.print(String(yAcceleration));
+  Serial.print(String(accY));
   Serial.print(",");
-  Serial.print(String(zAcceleration));
+  Serial.print(String(accZ));
   Serial.print(",");
-
-  sensorValue = analogRead(A1);
   Serial.print(sensorValue);
   Serial.println(",");
 
@@ -160,6 +110,7 @@ void calibrateSensor() {
   zeroGVoltageY = yTotal / numReadings;
   zeroGVoltageZ = zTotal / numReadings;
 
+  //Printing the calibration result
   /*
   Serial.print("Calibrated zero-g voltage for X: ");
   Serial.println(zeroGVoltageX);
